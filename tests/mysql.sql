0f59086339ab6ef2eb878595baa1a3b422b4dbe0
Skuza <skuza@emonit.de>
2017-06-06 14:27:28 +0200

    test

--- tests/mysql.sql
+++ tests/mysql.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `users` (
   `id`          INT(10) UNSIGNED AUTO_INCREMENT            NOT NULL,
   `email`        VARCHAR(128)                         NOT NULL,
-  `password`        VARBINARY(128)                         NOT NULL,
+  `password`        VARCHAR(256)                         NOT NULL,
   PRIMARY KEY (`id`),
   INDEX `select` (`email`)
 )
