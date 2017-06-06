418b3b3160ae54a82fd618753df7140d426e1f86
Skuza <skuza@emonit.de>
2017-06-06 13:07:34 +0200

    started register

--- src/php_user/user.php
+++ src/php_user/user.php
@@ -4,32 +4,68 @@
 
 namespace php_user;
 
+use Egulias\EmailValidator\EmailValidator;
+use Egulias\EmailValidator\Validation\RFCValidation;
+use ZxcvbnPhp\Zxcvbn;
+
 class user
 {
-    protected $session = null;
+    protected $session;
 
-    public function __construct(\php_session\session $session)
+    protected $db;
+
+    protected $minimum_password_strength_zxcvbn;
+
+    public function __construct(\php_session\session $session, \ParagonIE\EasyDB\EasyDB $db, int $minimum_password_strength_zxcvbn = 1)
     {
         $this->session = $session;
+
+        $this->db = $db;
+
+        $this->minimum_password_strength_zxcvbn = $minimum_password_strength_zxcvbn;
     }
 
     public function login()
     {
-
     }
 
     public function checklogin()
     {
-
     }
 
     public function register(string $email, string $password)
     {
+        if(empty($email)) {
+            //no email
+            return false;
+        }
 
+        if(empty($password)) {
+            //no password
+            return false;
+        }
+
+        $validator = new EmailValidator();
+        if(!$validator->isValid($email, new RFCValidation())) {
+            //not valid
+            return false;
+        }
+
+        $zxcvbn = new Zxcvbn();
+        $strength = $zxcvbn->passwordStrength($password, [$email]);
+        if($strength['score'] <= $this->minimum_password_strength_zxcvbn) {
+            //too weak
+            return false;
+        }
+
+        //check if email is taken
+        if($this->db->cell('SELECT id FROM users WHERE email = ?', $email)) {
+            // already taken
+            return false;
+        }
     }
 
     public function logout()
     {
-
     }
 }
