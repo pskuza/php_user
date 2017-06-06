42d264254db4cdc5a5d504dd08c6db1f3852c191
Skuza <skuza@emonit.de>
2017-06-06 23:51:23 +0200

    enable tests again

--- tests/UserMysqlTest.php
+++ tests/UserMysqlTest.php
@@ -14,24 +14,18 @@ class UserMysqlTest extends TestCase
 
         $r = $client->request('GET', 'http://127.0.0.1:8080/UserMysql.php?tests=0');
 
-        var_dump($r->getBody()->getContents());
-
-        //$this->assertEquals('0', $r->getBody()->getContents(), 'Invalid email was taken for registration.');
+        $this->assertEquals('0', $r->getBody()->getContents(), 'Invalid email was taken for registration.');
 
         $r = $client->request('GET', 'http://127.0.0.1:8080/UserMysql.php?tests=1');
 
-        var_dump($r->getBody()->getContents());
-
-        //$this->assertEquals('0', $r->getBody()->getContents(), 'Weak password was taken for registration.');
+        $this->assertEquals('0', $r->getBody()->getContents(), 'Weak password was taken for registration.');
 
         $r = $client->request('GET', 'http://127.0.0.1:8080/UserMysql.php?tests=2');
 
-        var_dump($r->getBody()->getContents());
-
-        //$this->assertEquals('1', $r->getBody()->getContents(), 'Could not register with valid email and strong password.');
+        $this->assertEquals('1', $r->getBody()->getContents(), 'Could not register with valid email and strong password.');
 
         $r = $client->request('GET', 'http://127.0.0.1:8080/UserMysql.php?tests=3');
 
-        var_dump($r->getBody()->getContents());
+        $this->assertEquals('1', $r->getBody()->getContents(), 'Could not login with valid email and password.');
     }
 }
