8984a5274eff5ff6f00b1ddc65f66380560d3ef4
pskuza <philip.skuza@gmail.com>
2017-06-12 00:03:40 +0200

    im dumb

--- tests/UserMysqlTest.php
+++ tests/UserMysqlTest.php
@@ -48,10 +48,6 @@ class UserMysqlTest extends TestCase
 
         $this->assertEquals('1', $r->getBody()->getContents(), 'Could not login with valid email and password.');
 
-        $r = $client->request('GET', 'http://127.0.0.1:8080/UserMysql.php?tests=3');
-
-        $this->assertEquals('1', $r->getBody()->getContents(), 'Could not login with valid email and password.');
-
         $r = $client->request('GET', 'http://127.0.0.1:8080/UserMysql.php?tests=5');
 
         $old_password = $r->getBody()->getContents();
